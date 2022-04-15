e90537a62ea982349689d308f03fb333f85158d2
Ivy Wang <i22062@hotmail.com>
2022-04-15 16:07:30 +0800

    Update Projects Part

--- src/components/MyProject/index.js
+++ src/components/MyProject/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import { projects } from "../../data/projects";
 
 function MyProject() {
   return (
@@ -8,32 +9,18 @@ function MyProject() {
         My Project
       </h3>
       <div className="pj-card-container">
-        <div className="pj-card">
-          <a href="/#">
-            <img
-              src="https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png"
-              alt="pj-img"
-            />
-          </a>
-          <span>title</span>
-          <div className="pj-link">
-            <a href="/#">Visit website</a>
-            <a href="/#">View Github</a>
+        {projects.map((p) => (
+          <div className="pj-card">
+            <a href={p.public_url}>
+              <img src={p.img} alt={p.title} />
+            </a>
+            <span>{p.title}</span>
+            <div className="pj-link">
+              <a href={p.public_url}>Visit website</a>
+              <a href={p.git_url}>View Github</a>
+            </div>
           </div>
-        </div>
-        <div className="pj-card">
-          <a href="/#">
-            <img
-              src="https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png"
-              alt="pj-img"
-            />
-          </a>
-          <span>title</span>
-          <div className="pj-link">
-            <a href="/#">Visit website</a>
-            <a href="/#">View Github</a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "My Task Board",
+    img: "/images/my-task-board-prj.jpg",
+    public_url: "/#",
+    git_url: "https://github.com/ccwang116/my-task-board",
+  },
+  {
+    title: "Project 1",
+    img: "/images/my-task-board-prj.jpg",
+    public_url: "",
+    git_url: "",
+  },
+  {
+    title: "Project 2",
+    img: "/images/my-task-board-prj.jpg",
+    public_url: "",
+    git_url: "https://github.com/ccwang116/my-task-board",
+  },
+];
